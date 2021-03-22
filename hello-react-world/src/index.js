ee3cf7cbd41916b3b2d8111d5ba1133f8ea29395
Samuel Pull <samuel.pull@accenture.com>
2021-03-22 11:19:13 +0100

    wip

--- hello-react-world/src/index.js
+++ hello-react-world/src/index.js
@@ -1,54 +1,32 @@
-// https://reactjs.org
 import React from 'react';
-// https://reactjs.org/docs/react-dom.html
 import ReactDOM from 'react-dom';
 
-// Vytvorte JSX element s nazvom 'element',
-// ktory obsahuje nasledovne prvky:
-// - h1 -> Name: meno
-// - p -> Town: mesto
-// - p -> Animal: oblubene zviera
-// - p -> Food: oblubene jedlo
-
-
-// JSX expression
-// premenne v JSX musia byt pouzite 
-// v *curly braces* zatvrokach
-// const name = 'Josh Perez';
-
-function makeGreeting(name) {
-  return "Hello, " + name + "!";
+//  Conditional rendering
+const futureWeather = {
+  location: "Trencin",
+  humidity: 60,
+  temperature: 20
 }
 
-// const element  = (
-//   <div>
-//     <p>{makeGreeting('albert')}</p>
-//   </div>
-// )
+const getTemperature = () => {
+  if (futureWeather.temperature) {
+    return <p>Temperature: {futureWeather.temperature}</p>
+  } else {
+    return <p></p>
+  }
+}
 
-const name = "Samuel"
-const town = "Stare mesto";
-const animal = "bunnies";
-const food = "pizza"
-
-const element = (
+const predpovedPocasia = (
   <div>
-    <h1>Name: {name}</h1>
-    <p>Town: {town}</p>
-    <p>Animal: {animal}</p>
-    <p>Food: {food}</p>
-  </div>
-)
-
-const prestavka = (
-  <div>
-    <h1>Prestavka do 10:40</h1>
-    <p>{makeGreeting('prestavka')}</p>
+    <h1>Weather in {futureWeather.location}</h1>
+    <p>Humidity: {futureWeather.humidity}</p>
+    {getTemperature()}
+    {futureWeather.temperature > 25 ? "😅" : "🥶"}
   </div>
 )
 
 ReactDOM.render(
-  prestavka,
+  predpovedPocasia,
   document.getElementById('root')
 );
 
