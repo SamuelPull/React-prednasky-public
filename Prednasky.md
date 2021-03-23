b8384bb6c4d5f221f03ffad0ad4fb3b36056e7d9
Samuel Pull <samuel.pull@accenture.com>
2021-03-23 10:46:48 +0100

    wip

--- Prednasky.md
+++ Prednasky.md
@@ -438,3 +438,52 @@ ReactDOM.render(
   document.getElementById('root')
 );
 ```
+
+### Riesenie
+```
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const info = {
+  title: 'TO DO list',
+  subtitle: 'Co mozes urobit zajtra, nerob dnes!',
+  todos: ['Upiect pizzu', 'Ist do lekarne']
+}
+
+const onFormSubmit = (e) => {
+  e.preventDefault(); // prevenet full page refresh
+
+  const todo = e.target.elements.newtodo.value;
+  if (todo) {
+    info.todos.push(todo);
+    e.target.elements.newtodo.value = "";
+    renderApp();
+    console.log(todo)
+    console.log(info.todos)
+  }
+}
+
+const renderApp = () => {
+  const app = (
+    <div>
+      <h1>{info.title}</h1>
+      {info.subtitle && <h2>{info.subtitle}</h2>}
+      <p>{(info.todos.length > 0) ? 
+      'Here is your todo list' : 
+      'Nothing to do'}</p>
+      <p>{info.todos.length}</p>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="newtodo" />
+        <button>Add TODO</button>
+      </form>
+    </div>
+  )
+
+  ReactDOM.render(
+      app,
+    document.getElementById('root')
+  );
+}
+
+renderApp()
+```
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Vypisat dlzku pola po pridani prvku, podobne ak v interaction-app
-// vyrenderovat aplikaciu
-// 1. Vytvorit renderovaciu funkciu
-// 2. Zavolat ju hned
-// 3. Zavolat ju po pridani todo
-
 const info = {
   title: 'TO DO list',
   subtitle: 'Co mozes urobit zajtra, nerob dnes!',
@@ -20,28 +14,32 @@ const onFormSubmit = (e) => {
   if (todo) {
     info.todos.push(todo);
     e.target.elements.newtodo.value = "";
-
+    renderApp();
     console.log(todo)
     console.log(info.todos)
   }
 }
 
-const app = (
-  <div>
-    <h1>{info.title}</h1>
-    {info.subtitle && <h2>{info.subtitle}</h2>}
-    <p>{(info.todos.length > 0) ? 
-    'Here is your todo list' : 
-    'Nothing to do'}</p>
-    <p>{info.todos.length}</p>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="newtodo" />
-      <button>Add TODO</button>
-    </form>
-  </div>
-)
+const renderApp = () => {
+  const app = (
+    <div>
+      <h1>{info.title}</h1>
+      {info.subtitle && <h2>{info.subtitle}</h2>}
+      <p>{(info.todos.length > 0) ? 
+      'Here is your todo list' : 
+      'Nothing to do'}</p>
+      <p>{info.todos.length}</p>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="newtodo" />
+        <button>Add TODO</button>
+      </form>
+    </div>
+  )
 
-ReactDOM.render(
-    app,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+      app,
+    document.getElementById('root')
+  );
+}
+
+renderApp()
